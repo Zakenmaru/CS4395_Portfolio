ca0ec01e339fc6221df3d6ef4d72d4215a1b7671
Soham Mukherjee <sohammukherjee2000@gmail.com>
2023-01-30 22:01:04 -0600

    Added valid number of args check

--- Homework1/Homework1_sxm180113.py
+++ Homework1/Homework1_sxm180113.py
@@ -4,10 +4,21 @@ sxm180113
 CS 4395 - Professor Mazidi
 1/30/2022
 """
+import sys
+
+
+def check_args():
+    if len(sys.argv) < 2:
+        print('ERROR: Please enter a filename as a system arg! Format: \'python Homework1_sxm180113.py '
+              '<filename>\'')
+        sys.exit(1)
+    else:
+        fp = sys.argv[1]
+        return fp
 
 
 def run_program():
-    print("Hey")
+    filename = check_args()
 
 
 if __name__ == '__main__':
