26e48e77c7176029f9dec019d70cc74c8e43f2f9
Soham Mukherjee <sohammukherjee2000@gmail.com>
2023-02-01 14:11:40 -0600

    People class created

--- Homework1/Homework1_sxm180113.py
+++ Homework1/Homework1_sxm180113.py
@@ -8,6 +8,20 @@ import os.path
 import sys
 
 
+class Person:
+    def __init__(self, last, first, mi, id, phone):
+        self.last = last
+        self.first = first
+        self.mi = mi
+        self.id = id
+        self.phone = phone
+
+    def display(self):
+        print("Employee id:", self.id)
+        print("\t\t", self.first, self.mi, self.last)
+        print("\t\t", self.phone)
+
+
 def check_args():
     # check if number of args is valid.
     if len(sys.argv) < 2:
