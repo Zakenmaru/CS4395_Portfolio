f218e5a2df6d16ddf82e432dfea5a5c02c9b34c8
Soham Mukherjee <sohammukherjee2000@gmail.com>
2023-02-11 18:18:08 -0600

    Finished guessing game first iteration code

--- Homework2/word_guess.py
+++ Homework2/word_guess.py
@@ -1,15 +1,71 @@
+import random
 import sys
 import os
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from collections import Counter
-
+from random import seed
+from random import randint
+seed(1234)
 
 nltk.download('punkt')
 nltk.download("stopwords")
 nltk.download("wordnet")
 nltk.download('averaged_perceptron_tagger')
+
+def guessing_game(words):
+    # start guessing game
+    print("Let's play a word guessing game!")
+
+    # initialize beginning points and done.
+    points = 5
+    done = False
+
+    # points flag makes sure that a valid amount of points are there to play.
+    # done ensures that the player hasn't quit yet.
+    while not done:
+        # choose a random word
+        rand_i = random.randint(0, len(words) - 1)
+        word = words[rand_i]
+
+        # initialize valid letters for the given word, and blanks
+        valid_letters = set(word)
+        blanks = ["_"] * len(word)
+        while True:
+            # if no more points, game over.
+            if points < 0:
+                print("Game over.")
+                done = True
+                break
+            # if the list is populated with anything other than underscores, then the word has been correctly guessed.
+            solved = True
+            for c in blanks:
+                if c == "_":
+                    solved = False
+                    break
+            if solved:
+                print("You solved it!")
+                print("\nCurrent Score: ", points)
+                print("Guess another word")
+                break
+            print(" ".join(blanks))
+            c = input("Guess a letter: ")
+            if c == "!":
+                done = True
+                break
+            if c not in valid_letters:
+                points -= 1
+                print("Sorry, guess again. Score is ", points)
+            else:
+                points += 1
+                blanks = [c if word[i] == c else blanks[i] for i in range(len(word))]
+                print("Right! Score is, ", points)
+    print("Thanks for playing!")
+
+
+
+
 
 
 def get_words(tokens, nouns):
@@ -93,6 +149,8 @@ def run_program():
     tokens, nouns = preprocess(filename)
     # get most common words
     guessing_words = get_words(tokens, nouns)
+    # play the guessing game
+    guessing_game(guessing_words)
 
 
 if __name__ == '__main__':
