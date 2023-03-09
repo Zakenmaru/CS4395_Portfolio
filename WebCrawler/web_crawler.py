94696497755e9d372f84b258500854c16eb98c0b
Soham Mukherjee <sohammukherjee2000@gmail.com>
2023-03-09 16:13:59 -0600

    fixed encoding error

--- WebCrawler/web_crawler.py
+++
@@ -1,9 +0,0 @@
-# Name: Shreya Valaboju, sxv180047
-# Name: Soham Mukherjee, sxm180113
-# Course/Section: CS 4395.001
-
-# Build a web crawler function that starts with a URL representing a topic (a sport, your
-# favorite film, a celebrity, a political issue, etc.) and outputs a list of at least 15 relevant
-# URLs. The URLs can be pages within the original domain but should have a few outside
-# the original domain.
-
